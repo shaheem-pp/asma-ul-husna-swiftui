ef56672eb81b7ce369a847b1ae157df66854036b
shaheem-pp <shanofficial2000@gmail.com>
2023-10-30 04:42:31 +0530

    initial commit

---
+++ asma-ul-husna-swiftui/asma-ul-husna-swiftui/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  asma-ul-husna-swiftui
+//
+//  Created by Shaheem PP on 30/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+        
+        var body: some View {
+            
+            Text("Hello World!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.accentColor)
+                .multilineTextAlignment(.center)  
+                
+        }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ asma-ul-husna-swiftui/asma-ul-husna-swiftui/asma_ul_husna_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  asma_ul_husna_swiftuiApp.swift
+//  asma-ul-husna-swiftui
+//
+//  Created by Shaheem PP on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct asma_ul_husna_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
